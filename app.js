632fd63a3d05d6f4e949f827935af8678634a8b7
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-11-10 23:45:20 +0100

    added function hide/show menu

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let toggleBtn = document.getElementById("toggle-button");
+    let navLinks = document.getElementsByClassName("nav-links")[0];
+    let logo = document.getElementById("logo");
+    let textCont = document.getElementsByClassName("text-content")[0];
+    let imgCont = document.getElementsByClassName("img-content")[0];
+
+    toggleBtn.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+        logo.classList.toggle("active");
+        textCont.classList.toggle("active");
+        imgCont.classList.toggle("active");
+    })
+})
