f19b3199cb84d416f637862710991ce11d341d87
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-11-10 23:52:47 +0100

    added function to open/close burger

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     let toggleBtn = document.getElementById("toggle-button");
+    let closeBtn = document.getElementById("close-button");
     let navLinks = document.getElementsByClassName("nav-links")[0];
     let logo = document.getElementById("logo");
     let textCont = document.getElementsByClassName("text-content")[0];
     let imgCont = document.getElementsByClassName("img-content")[0];
 
     toggleBtn.addEventListener("click", () => {
+        closeBtn.classList.toggle("active");
+        navLinks.classList.toggle("active");
+        logo.classList.toggle("active");
+        textCont.classList.toggle("active");
+        imgCont.classList.toggle("active");
+    })
+
+    closeBtn.addEventListener("click", () => {
+        closeBtn.classList.toggle("active");
         navLinks.classList.toggle("active");
         logo.classList.toggle("active");
         textCont.classList.toggle("active");
