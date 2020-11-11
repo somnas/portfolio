c438a648d5e968e20d14724f9a871ea0a76c8f4e
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-11-12 00:45:36 +0100

    built about page

---
+++ js/about.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let toggleBtn = document.getElementById("toggle-button");
+    let closeBtn = document.getElementById("close-button");
+    let navLinks = document.getElementsByClassName("nav-links")[0];
+    let logo = document.getElementById("logo");
+    let aboutCont = document.getElementsByClassName("about-content")[0];
+
+    toggleBtn.addEventListener("click", () => {
+        closeBtn.classList.toggle("active");
+        navLinks.classList.toggle("active");
+        logo.classList.toggle("active");    
+        aboutCont.classList.toggle("active");    
+    })
+
+    closeBtn.addEventListener("click", () => {
+        closeBtn.classList.toggle("active");
+        navLinks.classList.toggle("active");
+        logo.classList.toggle("active");
+        aboutCont.classList.toggle("active");       
+    })
+})
